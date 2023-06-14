5057885f9e2b5da2520969132f07c8fbd7d29018
Chekhrii Nazar <chekhrii_ak21@nuwm.edu.ua>
2023-06-15 02:59:17 +0300

    Курсова

---
+++ курсова/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace курсова
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.Contours = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Minute = new System.Windows.Forms.TextBox();
+            this.Second1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(655, 426);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // Open
+            // 
+            this.Open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Open.Location = new System.Drawing.Point(673, 256);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(115, 41);
+            this.Open.TabIndex = 1;
+            this.Open.Text = "Вибрати файл";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Contours
+            // 
+            this.Contours.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Contours.Location = new System.Drawing.Point(673, 303);
+            this.Contours.Name = "Contours";
+            this.Contours.Size = new System.Drawing.Size(115, 41);
+            this.Contours.TabIndex = 2;
+            this.Contours.Text = "Контур";
+            this.Contours.UseVisualStyleBackColor = true;
+            this.Contours.Click += new System.EventHandler(this.Contours_Click);
+            // 
+            // Save
+            // 
+            this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Save.Location = new System.Drawing.Point(673, 397);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(115, 41);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Зберегти";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Minute
+            // 
+            this.Minute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Minute.Location = new System.Drawing.Point(673, 31);
+            this.Minute.Name = "Minute";
+            this.Minute.Size = new System.Drawing.Size(115, 22);
+            this.Minute.TabIndex = 4;
+            // 
+            // Second1
+            // 
+            this.Second1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Second1.Location = new System.Drawing.Point(673, 76);
+            this.Second1.Name = "Second1";
+            this.Second1.Size = new System.Drawing.Size(115, 22);
+            this.Second1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(673, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Хвилини:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(673, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Секунди:";
+            // 
+            // Clear
+            // 
+            this.Clear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Clear.Location = new System.Drawing.Point(673, 350);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(115, 41);
+            this.Clear.TabIndex = 8;
+            this.Clear.Text = "Очистити";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Second1);
+            this.Controls.Add(this.Minute);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Contours);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "OpenCV";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Contours;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox Minute;
+        private System.Windows.Forms.TextBox Second1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ курсова/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace курсова
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture videoCapture;
+        private Bitmap originalFrame;
+        private Bitmap editedFrame;
+        private bool isVideoPlaying;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp|Video Files|*.mp4;*.avi;*.mov;*.mkv;*.wmv";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog.FilterIndex == 1)
+                {
+                    originalFrame = new Bitmap(openFileDialog.FileName);
+                    videoCapture = null;
+                    pictureBox.Image = originalFrame;
+                }
+                else if (openFileDialog.FilterIndex == 2)
+                {
+                    videoCapture = new VideoCapture(openFileDialog.FileName);
+                    originalFrame = null;
+                    editedFrame = null;
+                    isVideoPlaying = false;
+                    Video();
+                }
+            }
+        }
+
+        private void Video()
+        {
+            if (!isVideoPlaying)
+            {
+                double minutes = Convert.ToDouble(Minute.Text);
+                double seconds = Convert.ToDouble(Second1.Text);
+                double totalSeconds = (minutes * 60) + seconds;
+
+                videoCapture.Set(VideoCaptureProperties.PosMsec, totalSeconds * 1000);
+                isVideoPlaying = true;
+            }
+
+            Mat frame = new Mat();
+            if (!videoCapture.Read(frame) || frame.Empty())
+            {
+                MessageBox.Show("Неможливо прочитати кадр відео", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            originalFrame = BitmapConverter.ToBitmap(frame);
+            editedFrame = null;
+            pictureBox.Image = originalFrame;
+        }
+
+        private void Contours_Click(object sender, EventArgs e)
+        {
+            if (originalFrame == null && (videoCapture == null || !videoCapture.IsOpened()))
+            {
+                MessageBox.Show("Спочатку відкрийте зображення або відео", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (originalFrame != null)
+            {
+                Mat srcImage = BitmapConverter.ToMat(originalFrame);
+                Mat grayImage = new Mat();
+                Cv2.CvtColor(srcImage, grayImage, ColorConversionCodes.BGR2GRAY);
+
+                Mat cannyImage = new Mat();
+                Cv2.Canny(grayImage, cannyImage, 50, 150);
+
+                Mat contoursImage = new Mat();
+                Cv2.CvtColor(cannyImage, contoursImage, ColorConversionCodes.GRAY2BGR);
+
+                OpenCvSharp.Point[][] contours;
+                HierarchyIndex[] hierarchy;
+
+                Cv2.FindContours(cannyImage, out contours, out hierarchy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+                Cv2.DrawContours(contoursImage, contours, -1, Scalar.Black, -1, LineTypes.Link8, hierarchy);
+
+                Cv2.BitwiseNot(cannyImage, cannyImage);
+                contoursImage.SetTo(Scalar.White, cannyImage);
+
+                editedFrame = BitmapConverter.ToBitmap(contoursImage);
+                pictureBox.Image = editedFrame;
+
+            }
+            else if (videoCapture != null && videoCapture.IsOpened())
+            {
+                Video();
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (editedFrame == null)
+            {
+                MessageBox.Show("Немає відредагованого зображення", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JPEG Image|*.jpg";
+            saveFileDialog.Title = "Зберегти відредаговане зображення";
+            saveFileDialog.ShowDialog();
+
+            if (saveFileDialog.FileName != "")
+            {
+                editedFrame.Save(saveFileDialog.FileName);
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {          
+                pictureBox.Image = null;
+                editedFrame = null;           
+        }
+    }
+}
